2b3aca4c9c877e13903c9e2049c62fdf9af74250
Dominik Croci <dominikrcroci@gmail.com>
2023-09-11 14:11:49 -0700

    mobile styling

--- src/App.js
+++ src/App.js
@@ -54,23 +54,25 @@ function Form({ onAdd }) {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select
-        value={itemCount}
-        onChange={(e) => setItemCount(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((e) => (
-          <option value={e} key={e}>
-            {e}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></input>
-      <button>Add</button>
+      <div>
+        <select
+          value={itemCount}
+          onChange={(e) => setItemCount(Number(e.target.value))}
+        >
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((e) => (
+            <option value={e} key={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></input>
+        <button>Add</button>
+      </div>
     </form>
   );
 }
@@ -109,17 +111,30 @@ function Stats({ items }) {
   const totalItemsCount = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = (numPacked / items.length) * 100;
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list!</em>{' '}
+      </p>
+    );
+  }
 
   return (
     <footer className="stats">
       <em>
-        💼 You have {totalItemsCount} {totalItemsCount === 1 ? 'item' : 'items'}{' '}
-        on your list and you{' '}
-        {numPacked < 1
-          ? 'have not yet packed any '
-          : `already packed ${numPacked}`}
-        {numPacked > 1 || numPacked === 0 ? ' items ' : ' item '}(
-        {numPacked ? percentage.toFixed(2) + '%' : 0 + '%'})
+        {percentage === 100
+          ? 'You got everything! Ready to go!'
+          : `💼 You have ${totalItemsCount} ${
+              totalItemsCount === 1 ? 'item' : 'items'
+            }
+        on your list and you 
+        ${
+          numPacked < 1
+            ? 'have not yet packed any '
+            : `already packed ${numPacked}`
+        }
+        ${numPacked > 1 || numPacked === 0 ? ' items ' : ' item '}(
+        ${numPacked ? percentage.toFixed(2) + '%' : 0 + '%'})`}
       </em>
     </footer>
   );
